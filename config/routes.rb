3705909db760bba1d7d2931c16c26ee8cbb80bbe
Clementii Cicali <clementii.cicali@gmail.com>
2016-07-04 17:51:09 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+
   get 'home/index' => 'home#index'
+
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
